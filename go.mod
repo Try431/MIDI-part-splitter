fdba71c4934707fc5f3f7025e34e574e5fce8327
Isaac Garza <garzai@alum.mit.edu>
2019-09-21 17:57:12 -0700

    Periodic commit

--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module github.com/Try431/acc-midi-splitter
 
 go 1.12
 
-replace github.com/algoGuy/EasyMIDI => github.com/Try431/EasyMIDI v1.0.0
+replace github.com/algoGuy/EasyMIDI => github.com/Try431/EasyMIDI v1.0.2
 
-require github.com/Try431/EasyMIDI v1.0.1
+require github.com/Try431/EasyMIDI v1.0.2
--- main.go
+++ main.go
@@ -5,12 +5,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/Try431/EasyMIDI/smf"
 	"github.com/Try431/EasyMIDI/smfio"
 )
 
-const volumeControllerNum = uint8(0x7)
+const volumeControllerNum = uint8(0x07)
 
 func main() {
 
@@ -20,43 +21,73 @@ func main() {
 
 	// Read and save midi to smf.MIDIFile struct
 	midi, err := smfio.Read(bufio.NewReader(file))
-
 	if err != nil {
-		fmt.Println(err)
+		log.Panicf("Failed to read MIDI file %v with error: %v", file, err)
 	}
 
-	// Print number of midi tracks
-	// fmt.Println(midi.GetTracksNum())
+	var currentTrack uint16
+	for currentTrack = 0; currentTrack < midi.GetTracksNum(); currentTrack++ {
+		track := midi.GetTrack(currentTrack)
+		// if there is no MIDI_EVENT in the track, there's nothing to change in this track
+		if isHeaderTrack(track) {
+			continue
+		}
 
-	// Get zero track
-	track := midi.GetTrack(0)
-
-	// Get all midi events via iterator
-	iter := track.GetIterator()
-
-	for iter.MoveNext() {
-		// fmt.Println(iter.GetValue())
-	}
-
-	track1 := midi.GetTrack(1)
-	iter1 := track1.GetIterator()
-
-	volumeMIDIEvent := createNewVolumeEvent(track1, 75)
-	fmt.Println(volumeMIDIEvent)
-	var i uint32
-	i = 0
-	for iter1.MoveNext() {
-		fmt.Println(iter1.GetValue())
-		if iter1.GetValue().GetStatus() == 0xB0 && iter1.GetValue().GetData()[0] == 0x07 {
-			fmt.Println(iter1.GetValue().GetChannel())
-			// fmt.Println("Data:", iter1.GetValue().GetData())
-			// newTrack := replaceEvent(track1, i, volumeMIDIEvent)
-			// fmt.Println("Event after at cur pos:", newTrack.GetAllEvents()[i])
+		// TESTING
+		if currentTrack == 2 {
 			break
 		}
-		i++
+
+		trackChannel := track.GetAllEvents()[0].GetChannel()
+		controlChangeStatus := 0xB0 + trackChannel
+
+		// Get all midi events via iterator
+		iter := track.GetIterator()
+		volumeMIDIEvent := createNewVolumeEvent(track, 75, trackChannel)
+
+		var eventPos = uint32(0)
+		var newTrack *smf.Track
+		var trackName string
+		for iter.MoveNext() {
+			if iter.GetValue().GetMetaType() == smf.MetaSequenceTrackName {
+				trackName = grabTrackName(iter.GetValue())
+			}
+			fmt.Println(iter.GetValue())
+			// TESTING
+			if iter.GetValue().GetStatus() == controlChangeStatus && iter.GetValue().GetData()[0] == volumeControllerNum {
+				newTrack = replaceEvent(track, eventPos, volumeMIDIEvent)
+				break
+			}
+			eventPos++
+		}
+
+		fmt.Println(newTrack)
 	}
 
+}
+
+func grabTrackName(e smf.Event) string {
+	var trackName string
+	for _, c := range e.GetData() {
+		char := fmt.Sprintf("%c", c)
+		trackName += char
+	}
+	return trackName
+}
+
+// checks if there are any MIDI events in the track
+func isHeaderTrack(track *smf.Track) bool {
+	allEvents := track.GetAllEvents()
+	headerEvent := true
+	for _, e := range allEvents {
+		// fmt.Println(e.GetData())
+		// fmt.Println(e.String())
+		if strings.HasPrefix(e.String(), "MIDI") {
+			headerEvent = false
+			break
+		}
+	}
+	return headerEvent
 }
 
 func replaceEvent(track *smf.Track, replacePos uint32, newEvent *smf.MIDIEvent) *smf.Track {
@@ -71,11 +102,10 @@ func replaceEvent(track *smf.Track, replacePos uint32, newEvent *smf.MIDIEvent) 
 	return updatedTrack
 }
 
-func createNewVolumeEvent(t *smf.Track, newVolume uint8) *smf.MIDIEvent {
-	trackChannel := t.GetAllEvents()[0].GetChannel()
+func createNewVolumeEvent(t *smf.Track, newVolume uint8, channel uint8) *smf.MIDIEvent {
 	var controlChangeStatus uint8
-	controlChangeStatus = 0xB0 + trackChannel
-	newVolumeMIDIEvent, err := smf.NewMIDIEvent(0, controlChangeStatus, trackChannel, volumeControllerNum, newVolume)
+	controlChangeStatus = 0xB0 + channel
+	newVolumeMIDIEvent, err := smf.NewMIDIEvent(0, controlChangeStatus, channel, volumeControllerNum, newVolume)
 	if err != nil {
 		log.Printf("Failed to create new MIDI event with error: %v", err)
 	}
