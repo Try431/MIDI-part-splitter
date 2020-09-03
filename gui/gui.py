62fdeaaef289b05dfbf6eed2ba65bd54b4606ca7
Isaac Garza <garzai@alum.mit.edu>
2020-09-02 21:27:57 -0700

    Working clear entries button, and now using ScrolledText boxes for scrollbar

--- gui/gui.py
+++ gui/gui.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import tkinter as tk
 import tkinter.filedialog as tkfd
+import tkinter.messagebox as tkmb
+import tkinter.scrolledtext as tksc
 import subprocess
 import os
 from functools import partial
@@ -20,7 +22,7 @@ class MIDIConvertGUI(object):
         self.label.pack()
         self.button_frame = tk.Frame(parent)
         self.button_frame.pack(side=tk.TOP)
-        self.files_to_convert_text_box = tk.Text(
+        self.files_to_convert_text_box = tksc.ScrolledText(
             width=150
         )
         self.choose_file_btn = tk.Button(
@@ -51,7 +53,7 @@ class MIDIConvertGUI(object):
             height=2,
             bg="red"
         )
-        self.log_box = tk.Text(
+        self.log_box = tksc.ScrolledText(
             width=100, 
             bg="black", 
             fg="white", 
@@ -69,7 +71,15 @@ class MIDIConvertGUI(object):
         if directory:
             files_in_dir = map(lambda p: os.path.join(directory, p), os.listdir(directory))
             self.add_file_to_text_box(files_in_dir)
-            
+        
+    def clear_all_entries(self, event):
+        # result = tk.messagebox.askquestion(title="Clear all entries", message="Are You Sure?", icon='warning')
+        result = tkmb.askquestion(title="Clear all entries", message="Are you sure?", icon='warning')
+        if result == 'yes':
+            self.files_to_convert_text_box.config(state="normal")
+            self.files_to_convert_text_box.delete("1.0", tk.END)
+            self.files_to_convert_text_box.config(state="disabled")
+        
             
     def remove_duplicates(self):
         line_set = set()
@@ -94,14 +104,19 @@ class MIDIConvertGUI(object):
                 files_in_dir = map(lambda p: os.path.join(path, p), os.listdir(path))
                 self.add_file_to_text_box(files_in_dir)
             elif os.path.isfile(path):
-                full_path, filename = os.path.split(path)
+                _, filename = os.path.split(path)
                 if not re.match("mid*", filename.partition(".")[2]):
                     self.log_box.insert(tk.END, f"{path} does not point to a MIDI file\n")
                 else:
-                    self.files_to_convert_text_box.config(state="normal")
-                    self.files_to_convert_text_box.insert(tk.END, path+"\n")
-                    self.remove_duplicates()
-                    self.files_to_convert_text_box.config(state="disabled")
+                    current_lines = self.files_to_convert_text_box.get('1.0', 'end-1c').splitlines()
+                    if path not in current_lines:
+                        self.files_to_convert_text_box.config(state="normal")
+                        self.files_to_convert_text_box.insert(tk.END, path+"\n")
+                        self.files_to_convert_text_box.config(state="disabled")
+                    else:
+                        self.log_box.config(state="normal")
+                        self.log_box.insert(tk.END, f"The file {filename} is already listed\n")
+                        self.log_box.config(state="disabled")
 
         
             
@@ -124,24 +139,16 @@ if __name__ == "__main__":
     
     gui.choose_file_btn.bind("<Button-1>", gui.select_file)
     gui.choose_dir_btn.bind("<Button-1>", gui.select_dir)
+    gui.clear_all_btn.bind("<Button-1>", gui.clear_all_entries)
     
+    gui.files_to_convert_text_box.config(state="disabled")
+    gui.log_box.config(state="disabled")
     root.mainloop()
     
     # Old code to convert
     
-    # files_to_convert_text_box = tk.Text(width=150)
-    # log_box = tk.Text(width=100, bg="black", fg="white", insertbackground="white")
-    # choose_file_btn.pack()
-    # choose_dir_btn.pack()
-    # clear_all_btn.pack()
-    # choose_file_btn.bind("<Button-1>", partial(select_file, files_to_convert_text_box, log_box))
-    # choose_dir_btn.bind("<Button-1>", partial(select_dir, files_to_convert_text_box, log_box))
     # start_conversion_btn.bind("<Button-1>", partial(begin_conversion, files_to_convert_text_box))
     # start_conversion_btn.pack()
-    # files_to_convert_text_box.config(state="disabled")
-    # files_to_convert_text_box.pack()
-    # log_box.pack()
-    # window.mainloop()
     
 # pp = pprint.PrettyPrinter(indent=2)
 # Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
