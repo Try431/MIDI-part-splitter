3b301000d1bddb6202f9eea29f2a348b532dc4fd
Isaac Garza <garzai@alum.mit.edu>
2020-09-07 21:02:55 -0700

    Cleaning up file verification and adding comma-separated list of files as valid flag input

--- main.go
+++ main.go
@@ -26,11 +26,11 @@ func main() {
 	volFlagPtr := flag.Int("vol", 40, "Volume of de-emphasized voice tracks - must be between 0 and 100\n(e.g., '"+binaryName+" -f midi_file.mid -vol 30)")
 	outFlagPtr := flag.String("o", "./"+midi.MIDIOutputDirectory+"/mp3s", "Directory where mp3 files will be stored\n(e.g., '"+binaryName+" -f midi_file.mid -o ./dir/to/store/mp3s)")
 	quietFlagPtr := flag.Bool("quiet", true, "Whether or not to silence standard output when running (will still allow stderr)")
-	// listFlagPtr := flag.String("l", "", "List of comma-separated files")
+	listFlagPtr := flag.String("l", "", "List of comma-separated MIDI files to be parsed")
 
 	flag.Parse()
 
-	if !isFlagPassed("f") && !isFlagPassed("d") {
+	if !(isFlagPassed("f") || isFlagPassed("d") || isFlagPassed("l")) {
 		flag.Usage()
 		os.Exit(1)
 	}
@@ -39,14 +39,6 @@ func main() {
 		midi.SilenceOutput = bool(*quietFlagPtr)
 	}
 
-	// if isFlagPassed("l") {
-	// 	// files := strings.Split(*listFlagPtr, ",")
-	// 	// for _, f := range files {
-	// 	// TODO - create helper function collectFilePaths that does what is done in the isFlagPassed("f") func, and use here and in that func to reduce code reuse
-	// 	// Also, make sure that calling without a ./ or ../ prefix still works, as well as absolute filepaths
-	// 	// }
-	// }
-
 	if isFlagPassed("vol") {
 		midi.NonEmphasizedTrackVolume = uint8(*volFlagPtr)
 	}
@@ -54,17 +46,17 @@ func main() {
 	if isFlagPassed("o") {
 		midi.MP3OutputDirectory = *outFlagPtr
 	}
-	var filePaths []string
 
 	if isFlagPassed("inst") {
 		midi.EmphasizedInstrumentNum = uint8(*instFlagPtr)
 	}
 
+	var filePaths []string
+
 	if isFlagPassed("f") {
 		filePath := filepath.Clean(*fileFlagPtr)
-		extension := filepath.Ext(filePath)
-		if !(strings.ToLower(extension) == ".mid" || strings.ToLower(extension) == ".midi") {
-			log.Panicf("Extension " + extension + " not accepted - only MIDI files allowed")
+		if !isMIDIFile(filePath) {
+			log.Fatal("File " + filePath + " not accepted - only MIDI files allowed")
 		}
 		filePaths = append(filePaths, filePath)
 	}
@@ -72,6 +64,17 @@ func main() {
 	if isFlagPassed("d") {
 		files := grabFilesInDir(*dirFlagPtr)
 		filePaths = append(filePaths, files...)
+	}
+
+	if isFlagPassed("l") {
+		files := strings.Split(*listFlagPtr, ",")
+		for _, f := range files {
+			filePath := filepath.Clean(f)
+			if !isMIDIFile(filePath) {
+				log.Fatal("File " + filePath + " not accepted - only MIDI files allowed")
+			}
+			filePaths = append(filePaths, filePath)
+		}
 	}
 
 	fmt.Println("Starting split & conversion process...")
@@ -96,19 +99,24 @@ func isFlagPassed(name string) bool {
 	return found
 }
 
+func isMIDIFile(path string) bool {
+	extension := filepath.Ext(path)
+	return (extension == ".mid" || extension == ".midi")
+}
+
 // Walks through directory recursively and grabs all MIDI files
 func grabFilesInDir(dirPath string) []string {
 	var files []string
 
 	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
-		if err == nil && (filepath.Ext(info.Name()) == ".mid" || filepath.Ext(info.Name()) == ".midi") {
+		if err == nil && isMIDIFile(info.Name()) {
 			filePath := filepath.Clean(path)
 			files = append(files, filePath)
 		}
 		return nil
 	})
 	if err != nil {
-		log.Panicf("Failed to walk through dirPath %v with error: %v", dirPath, err)
+		log.Fatalf("Failed to walk through dirPath %v with error: %v", dirPath, err)
 	}
 	return files
 }
