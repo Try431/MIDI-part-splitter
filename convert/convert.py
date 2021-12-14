607f23ee733f7dcbb43aa4ddec76d110b071a044
Isaac Garza <garzai@alum.mit.edu>
2021-12-14 12:41:44 -0600

    Working to get python midi to mp3 code fit for lambda

--- convert/convert.py
+++
@@ -1,9 +0,0 @@
-from midi2audio import FluidSynth
-from pydub import AudioSegment
-
-fs = FluidSynth(sound_font="convert/FluidR3_GM.sf2")
-output_wav = "convert/test_output.wav"
-fs.midi_to_audio(midi_file="convert/Hine_Ma_Tov_Bass.mid", audio_file=output_wav)
-
-wav_audio = AudioSegment.from_file(output_wav, format="wav")
-wav_audio.export("convert/test_output.mp3", format="mp3")
